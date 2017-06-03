ad1f3f92bb3e4b95e580d3a22f2d2a9cba8f0ad3
Konstantin Kharitonov <konstantin.kharitonov@gmail.com>
2017-06-03 22:51:05 +0300

    Migration to Kotlin

---
+++ widget/src/main/java/ru/kest/trainswidget/Permissions.kt
@@ -0,0 +1,49 @@
+package ru.kest.trainswidget
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+
+import ru.kest.trainswidget.Constants.LOG_TAG
+
+/**
+ * Permission checker and handler
+ */
+class Permissions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(baseContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(LOG_TAG, "Permissions do not exists")
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1)
+        } else {
+            Log.i(LOG_TAG, "Permissions exists")
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        Log.i(LOG_TAG, "Permissions.onRequestPermissionsResult()")
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(baseContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(baseContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
---
+++ widget/src/main/java/ru/kest/trainswidget/PopUpActivity.kt
@@ -0,0 +1,81 @@
+package ru.kest.trainswidget
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import ru.kest.trainswidget.Constants.*
+
+/**
+ * Activity for processing train selection
+ *
+ * Created by Konstantin on 27.05.2017.
+ */
+class PopUpActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setBackgroundDrawable(ColorDrawable(0))
+
+        Log.i(LOG_TAG, "PopUpActivity.onCreate()")
+        // Should do a proper argument verification here
+        val extras = intent.extras
+        if (extras != null && extras.containsKey(DETAILS)) {
+            displayDialog(extras)
+        } else {
+            Log.w(LOG_TAG, "PopUpActivity: detail for dialog not found, finishing")
+            finish()
+        }
+    }
+
+    private fun displayDialog(extras: Bundle) {
+        val details = extras.getString(DETAILS)
+        AlertDialog.Builder(this)
+                .setTitle("Расписание электричек")
+                .setMessage("Следить за рейсом $details?")
+                .setPositiveButton(android.R.string.yes) { dialog, _ ->
+                    //                Intent onClickIntent = new Intent(context, TrainsWidget.class);
+                    val notificationIntent = Intent(this@PopUpActivity, TrainsWidget::class.java)
+                    notificationIntent.action = CREATE_NOTIFICATION
+                    notificationIntent.putExtra(RECORD_HASH, extras.getInt(RECORD_HASH))
+                    sendBroadcast(notificationIntent)
+
+                    // Handle a positive answer
+                    dialog.dismiss()
+                    finish()
+                }
+                .setNegativeButton(android.R.string.no) { dialog, _ ->
+                    // Handle a negative answer
+                    dialog.dismiss()
+                    finish()
+                }
+                .setIcon(R.mipmap.ic_launcher)
+                .setOnDismissListener { finish() }
+                .show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_pop_up, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
