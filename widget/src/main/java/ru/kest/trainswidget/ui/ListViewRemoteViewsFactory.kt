ff5f2475d9b3ebf72c5c21417daa7a4f81b262d8
Konstantin Kharitonov <konstantin.kharitonov@gmail.com>
2017-06-04 23:51:23 +0300

    Scrollable widget: 4 elements in line

--- widget/src/main/java/ru/kest/trainswidget/ui/ListViewRemoteViewsFactory.kt
+++ widget/src/main/java/ru/kest/trainswidget/ui/ListViewRemoteViewsFactory.kt
@@ -15,6 +15,35 @@ import ru.kest.trainswidget.R
  */
 class ListViewRemoteViewsFactory(context : Context, intent : Intent?) : RemoteViewsService.RemoteViewsFactory {
 
+    override fun getViewAt(position: Int): RemoteViews {
+        Log.d(LOG_TAG, "ListViewRemoteViewsFactory.getViewAt($position)")
+        val remoteView = RemoteViews(PACKAGE_NAME, R.layout.trains_line)
+        for(i in 1..4) {
+            remoteView.addView(R.id.line_ll, getNestedView(i))
+        }
+        return remoteView
+    }
+
+    private fun getNestedView(num : Int) : RemoteViews {
+        val remoteView = RemoteViews(PACKAGE_NAME, R.layout.train_item)
+        remoteView.setTextViewText(R.id.from, "from $num")
+        remoteView.setTextViewText(R.id.to, "to $num")
+        return remoteView
+
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 1
+    }
+
+    override fun onDestroy() {
+        Log.d(LOG_TAG, "ListViewRemoteViewsFactory.onDestroy()")
+    }
+
     override fun onCreate() {
     }
 
@@ -34,24 +63,5 @@ class ListViewRemoteViewsFactory(context : Context, intent : Intent?) : RemoteVi
         return true
     }
 
-    override fun getViewAt(position: Int): RemoteViews {
-        Log.d(LOG_TAG, "ListViewRemoteViewsFactory.getViewAt($position)")
-        val remoteView = RemoteViews(PACKAGE_NAME, R.layout.train_item)
-        remoteView.setTextViewText(R.id.from, "from")
-        remoteView.setTextViewText(R.id.to, "to")
-        return remoteView
-    }
-
-    override fun getCount(): Int {
-        return 2
-    }
-
-    override fun getViewTypeCount(): Int {
-        return 1
-    }
-
-    override fun onDestroy() {
-        Log.d(LOG_TAG, "ListViewRemoteViewsFactory.onDestroy()")
-    }
 
 }
